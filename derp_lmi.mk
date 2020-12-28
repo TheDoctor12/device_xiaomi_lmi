5d1925fb218ac80c579807e598188398395b277d
RealSchnedi <schnedi3@gmail.com>
2020-12-28 18:48:07 +0100

    [DNM] lmi: Derp bringup

--- derp_lmi.mk
+++ derp_lmi.mk
@@ -6,11 +6,17 @@
 
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-# Inherit some common aicp stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Derp stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Derp specific properties
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.derp.maintainer=Schnedi \
+  ro.derp.cpu=sm8250 \
+  ro.derp.rom=Derp
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_lmi
+PRODUCT_NAME := derp_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := F2 Pro
