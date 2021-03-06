fe34617f77e54d94d783fa655b0df18b7953cdd7
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-03-06 20:13:30 +0100

    lmi: Build more display libs

--- device.mk
+++ device.mk
@@ -73,12 +73,18 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     libdisplayconfig.qti \
     libdisplayconfig.qti.vendor \
     libqdMetaData \
     libqdMetaData.system \
+    libtinyxml \
     libvulkan \
-    vendor.display.config@1.0
+    vendor.display.config@1.0 \
+    vendor.qti.hardware.display.allocator-service
 
 # Fastbootd
 PRODUCT_PACKAGES += \
