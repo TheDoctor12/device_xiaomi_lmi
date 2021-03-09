5ea0ea915a85adea9958447164303bc3f0961432
RealSchnedi <schnedi3@gmail.com>
2021-03-09 20:05:06 +0100

    lmi: Move overlay-lmi to overlay

--- device.mk
+++ device.mk
@@ -152,10 +152,9 @@ PRODUCT_PACKAGES += \
     vendor.nxp.hardware.nfc@1.1 \
     vendor.nxp.hardware.nfc@2.0
 
-# Overlays
+# Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lmi
+    $(LOCAL_PATH)/overlay
 
 # Perf
 PRODUCT_PACKAGES += \
