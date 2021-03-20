Linvn/device_xiaomi_lmi-prebuild

lmi: Refactor custom init
* Make it simpler

Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
Co-authored-by: Lucchetto <lucchetto@gmail.com>

init/init_lmi.cpp
@@ -1,142 +1,58 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
-   Copyright (C) 2016 The CyanogenMod Project.
-   Copyright (C) 2019-2020 The LineageOS Project.
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <fstream>
-#include <unistd.h>
-#include <vector>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include "property_service.h"
 #include "vendor_init.h"
+#include "property_service.h"
 
 using android::base::GetProperty;
 
-int property_set(const char *key, const char *value) {
-    return __system_property_set(key, value);
-}
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
 
-constexpr const char *RO_PROP_SOURCES[] = {
-    nullptr,   "product.", "product_services.", "odm.",
-    "vendor.", "system.", "system_ext.", "bootimage.",
-};
-
-constexpr const char *BRANDS[] = {
-    "Redmi",
-    "POCO",
-};
-
-constexpr const char *PRODUCTS[] = {
-    "lmi",
-    "lmi",
-};
-
-constexpr const char *DEVICES[] = {
-    "Redmi K30 Pro",
-    "POCO F2 Pro",
-};
-
-constexpr const char *BUILD_DESCRIPTION[] = {
-    "redfin-user 11 RQ1A.210105.003 7005429 release-keys",
-    "redfin-user 11 RQ1A.210105.003 7005429 release-keys",
-};
-
-constexpr const char *BUILD_FINGERPRINT[] = {
-    "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys",
-    "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys",
-};
-
-constexpr const char *CLIENT_ID[] = {
-    "android-xiaomi",
-    "android-xiaomi",
-};
-
-void property_override(char const prop[], char const value[], bool add = true) {
-  prop_info *pi;
-
-  pi = (prop_info *)__system_property_find(prop);
-  if (pi)
-    __system_property_update(pi, value, strlen(value));
-  else if (add)
-    __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-void load_props(const char *model, bool is_in = false) {
-  const auto ro_prop_override = [](const char *source, const char *prop,
-                                   const char *value, bool product) {
-    std::string prop_name = "ro.";
-
-    if (product)
-      prop_name += "product.";
-    if (source != nullptr)
-      prop_name += source;
-    if (!product)
-      prop_name += "build.";
-    prop_name += prop;
-
-    property_override(prop_name.c_str(), value);
-  };
-
-  for (const auto &source : RO_PROP_SOURCES) {
-    ro_prop_override(source, "device", is_in ? PRODUCTS[1] : PRODUCTS[0], true);
-    ro_prop_override(source, "model", model, true);
-    if (!is_in) {
-      ro_prop_override(source, "brand", BRANDS[0], true);
-      ro_prop_override(source, "name", PRODUCTS[0], true);
-      ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[0], false);
-    } else {
-      ro_prop_override(source, "brand", BRANDS[1], true);
-      ro_prop_override(source, "name", PRODUCTS[1], true);
-      ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[1], false);
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
     }
-  }
-
-  if (!is_in) {
-    ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[0], false);
-    property_override("ro.boot.product.hardware.sku", PRODUCTS[0]);
-  } else {
-    ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[1], false);
-    property_override("ro.com.google.clientidbase", CLIENT_ID[0]);
-    property_override("ro.com.google.clientidbase.ms", CLIENT_ID[1]);
-  }
-  ro_prop_override(nullptr, "product", model, false);
 }
 
 void vendor_load_properties() {
-  std::string region;
-  region = GetProperty("ro.boot.hwc", "");
+    std::string region = GetProperty("ro.boot.hwc", "");
+    std::string product = GetProperty("ro.boot.product.hardware.sku", "");
 
-  if (region == "CN") {
-    load_props(DEVICES[0], false);
-  } else if (region == "GLOBAL") {
-    load_props(DEVICES[1], true);
-  }
+    property_override("ro.oem_unlock_supported", "0");
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210305.006 7119741 release-keys");
+    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys");
+
+    if (region.find("CN") != std::string::npos) {
+        property_override("ro.product.brand", "Xiaomi");
+        if (product.find("pro") != std::string::npos) {
+            property_override("ro.product.model", "Redmi K30 Pro Zoom Edition");
+            property_override("ro.product.device", "lmipro");
+        } else {
+            property_override("ro.product.model", "Redmi K30 Pro");
+        }
+    } else if (region.find("GLOBAL") != std::string::npos) {
+        property_override("ro.product.model", "POCO F2 Pro");
+    }
 }

TheDoctor12/device_xiaomi_lmi

init/init_lmi.cpp
@@ -1,142 +1,58 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
-   Copyright (C) 2016 The CyanogenMod Project.
-   Copyright (C) 2019-2020 The LineageOS Project.
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <fstream>
-#include <unistd.h>
-#include <vector>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include "property_service.h"
 #include "vendor_init.h"
+#include "property_service.h"
 
 using android::base::GetProperty;
 
-int property_set(const char *key, const char *value) {
-    return __system_property_set(key, value);
-}
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
 
-constexpr const char *RO_PROP_SOURCES[] = {
-    nullptr,   "product.", "product_services.", "odm.",
-    "vendor.", "system.", "system_ext.", "bootimage.",
-};
-
-constexpr const char *BRANDS[] = {
-    "Redmi",
-    "POCO",
-};
-
-constexpr const char *PRODUCTS[] = {
-    "lmi",
-    "lmi",
-};
-
-constexpr const char *DEVICES[] = {
-    "Redmi K30 Pro",
-    "POCO F2 Pro",
-};
-
-constexpr const char *BUILD_DESCRIPTION[] = {
-    "redfin-user 11 RQ1A.210105.003 7005429 release-keys",
-    "redfin-user 11 RQ1A.210105.003 7005429 release-keys",
-};
-
-constexpr const char *BUILD_FINGERPRINT[] = {
-    "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys",
-    "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys",
-};
-
-constexpr const char *CLIENT_ID[] = {
-    "android-xiaomi",
-    "android-xiaomi",
-};
-
-void property_override(char const prop[], char const value[], bool add = true) {
-  prop_info *pi;
-
-  pi = (prop_info *)__system_property_find(prop);
-  if (pi)
-    __system_property_update(pi, value, strlen(value));
-  else if (add)
-    __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-void load_props(const char *model, bool is_in = false) {
-  const auto ro_prop_override = [](const char *source, const char *prop,
-                                   const char *value, bool product) {
-    std::string prop_name = "ro.";
-
-    if (product)
-      prop_name += "product.";
-    if (source != nullptr)
-      prop_name += source;
-    if (!product)
-      prop_name += "build.";
-    prop_name += prop;
-
-    property_override(prop_name.c_str(), value);
-  };
-
-  for (const auto &source : RO_PROP_SOURCES) {
-    ro_prop_override(source, "device", is_in ? PRODUCTS[1] : PRODUCTS[0], true);
-    ro_prop_override(source, "model", model, true);
-    if (!is_in) {
-      ro_prop_override(source, "brand", BRANDS[0], true);
-      ro_prop_override(source, "name", PRODUCTS[0], true);
-      ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[0], false);
-    } else {
-      ro_prop_override(source, "brand", BRANDS[1], true);
-      ro_prop_override(source, "name", PRODUCTS[1], true);
-      ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[1], false);
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
     }
-  }
-
-  if (!is_in) {
-    ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[0], false);
-    property_override("ro.boot.product.hardware.sku", PRODUCTS[0]);
-  } else {
-    ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[1], false);
-    property_override("ro.com.google.clientidbase", CLIENT_ID[0]);
-    property_override("ro.com.google.clientidbase.ms", CLIENT_ID[1]);
-  }
-  ro_prop_override(nullptr, "product", model, false);
 }
 
 void vendor_load_properties() {
-  std::string region;
-  region = GetProperty("ro.boot.hwc", "");
+    std::string region = GetProperty("ro.boot.hwc", "");
+    std::string product = GetProperty("ro.boot.product.hardware.sku", "");
 
-  if (region == "CN") {
-    load_props(DEVICES[0], false);
-  } else if (region == "GLOBAL") {
-    load_props(DEVICES[1], true);
-  }
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210305.006 7119741 release-keys");
+    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys");
+
+    if (region.find("CN") != std::string::npos) {
+        property_override("ro.product.brand", "Redmi");
+
+        if (product.find("pro") != std::string::npos) {
+            property_override("ro.product.model", "Redmi K30 Pro Zoom Edition");
+            property_override("ro.product.device", "lmipro");
+        } else {
+            property_override("ro.product.model", "Redmi K30 Pro");
+        }
+    } else if (region.find("GLOBAL") != std::string::npos) {
+        property_override("ro.product.model", "POCO F2 Pro");
+    }
 }