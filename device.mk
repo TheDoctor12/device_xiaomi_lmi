2e6df5838a44b2c8737cdb83c7c7ed315afd3cc9
TheDoctor12 <jkaskabas95@gmail.com>
2021-03-22 19:50:48 +0200

    lmi: Restore DevicesAndroidOverlay overlay-remove
    aosp corner mask config won't work without it

--- device.mk
+++ device.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# overlay-remove
+PRODUCT_PACKAGES += \
+    DevicesAndroidOverlay
+
 # Perf
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2
